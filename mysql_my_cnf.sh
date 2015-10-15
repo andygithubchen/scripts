1fe591e7e91a99c94917bdb6961017c29d41fc93
andychen <bootoo@sina.cn>
2015-10-15 14:54:07 +0800

    modifiy mysql_my_cnf.sh

--- mysql_my_cnf.sh
+++ mysql_my_cnf.sh
@@ -6,9 +6,13 @@
 #   重启mysql：
 #   /etc/init.d/mysql restart;
 #
+#
 #2：进入mysql 数据库
 #   mysql -u  root -p
 #   mysql>GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '123456' WITH GRANT OPTION;
 #   其中 第一个*表示数据库名；第二个*表示该数据库的表名；如果像上面那样 *.*的话表示所有到数据库下到所有表都允许访问；
 #   ‘%’：表示允许访问到mysql的ip地址；当然你也可以配置为具体到ip名称；%表示所有ip均可以访问；
 #   后面到‘xxxx’为root 用户的password；
+#
+#
+#3：关闭防火墙！
