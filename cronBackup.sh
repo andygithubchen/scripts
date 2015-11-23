17366ad1d5c0219b29d500a5f407b2165a7be1bb
root <root@localhost.localdomain>
2015-11-23 09:33:56 +0800

    modify cronBackup.sh file

--- cronBackup.sh
+++ cronBackup.sh
@@ -4,16 +4,29 @@
 # Date  : Sat Apr 11 19:05:12 CST 2015
 # Author: andychen (bootoo@sina.cn)
 
+#要备份的项目所在路径
+itemPath=/home/www/ 
 
-#year=`date +"%Y"`
-#month=`date +"%m"`
-#day=`date +"%d"`
-#
-#path=`mkdir -p /home/andy/crontab/${year}/${month}/${day}`
-#cd /home/andy/crontab/${year}/${month}/${day}
+#备份文件的存放路径
+itemPath=/home/andy/backup
+
+cd ${itemPath}
+
+year=`date +"%Y"`
+month=`date +"%m"`
+day=`date +"%d"`
+
+mkdir -p ${itemPath}/${year}/${month}/${day}
+
+
+for file in `ls`; do
+     tar -zcvf ${itemPath}/${year}/${month}/${day}/${file}.tar.gz ${file}
+done 
+
+
+cd - >> /dev/null
 
 
 # at 4 a.m every day with:
 # m h dom mon dow   command
 # 0 4 * * * /home/andy/crontab.sh
-
