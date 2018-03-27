f4c69f96339720d81efad5757f3d4aef586848dd
andychen <bootoo@sina.cn>
2018-03-27 02:21:06 -0700

    fix

--- golang-install.sh
+++ golang-install.sh
@@ -7,7 +7,7 @@ tar -C /usr/local/ -zxvf go1.8.3.linux-amd64.tar.gz
 mkdir /Golang
 
 
-cat > /etc/profile <<END
+cat >> /etc/profile <<END
 export GOROOT=/usr/local/go
 export GOBIN=$GOROOT/bin
 export GOPKG=$GOROOT/pkg/tool/linux_amd64
