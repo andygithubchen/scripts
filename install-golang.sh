77edf7b7036ae9528435c32caef87f1f8a726cde
andychen <bootoo@sina.cn>
2020-01-14 11:09:52 +0800

    fix

--- install-golang.sh
+++ install-golang.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#wget -c https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-#
-#tar -C /usr/local/ -zxvf go1.8.3.linux-amd64.tar.gz
-#
-#mkdir /Golang
+wget -c https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz
+
+tar -C /usr/local/ -zxvf go1.13.6.linux-amd64.tar.gz
+
+#mkdir /golang
 #
 #cat >> /etc/profile <<END
 #
@@ -13,36 +13,9 @@
 #export GOPKG=\$GOROOT/pkg/tool/linux_amd64
 #export GOARCH=amd64
 #export GOOS=linux
-#export GOPATH=/Golang
+#export GOPATH=/golang
 #export PATH=\$PATH:\$GOBIN:\$GOPKG:\$GOPATH/bin
 #END
 
-
-
-
-
-#ubuntu 16
-sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt-get update
-sudo apt-get install golang-go
-
-#centOS
-#yum install epel -y
-#yum install go -y
-
-
-
-cat >> /etc/profile <<END
-
-export GOROOT=/usr/local/go
-export GOBIN=$GOROOT/bin
-export GOPKG=$GOROOT/pkg/tool/linux_amd64
-export GOARCH=amd64
-export GOOS=linux
-export GOPATH=/golang
-export PATH=$PATH:$GOBIN:$GOPKG:$GOPATH/bin
-END
-
-source /etc/profile
 
 go version
