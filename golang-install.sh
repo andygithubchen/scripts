36712945596f1cdb05be616c842338e360a5e657
andychen <bootoo@sina.cn>
2021-06-25 10:35:42 +0800

    fix

--- golang-install.sh
+++ golang-install.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-#wget -c https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-#
-#tar -C /usr/local/ -zxvf go1.8.3.linux-amd64.tar.gz
-#
-#mkdir /Golang
-#
-#cat >> /etc/profile <<END
-#
-#export GOROOT=/usr/local/go
-#export GOBIN=\$GOROOT/bin
-#export GOPKG=\$GOROOT/pkg/tool/linux_amd64
-#export GOARCH=amd64
-#export GOOS=linux
-#export GOPATH=/Golang
-#export PATH=\$PATH:\$GOBIN:\$GOPKG:\$GOPATH/bin
-#END
+wget -c https://mirrors.nju.edu.cn/golang/go1.13.linux-amd64.tar.gz
 
+tar -C /usr/local/ -zxvf go1.13.linux-amd64.tar.gz
 
+mkdir /golang
 
+cat >> /etc/profile <<END
+
+export GOROOT=/usr/local/go
+export GOBIN=\$GOROOT/bin
+export GOPKG=\$GOROOT/pkg/tool/linux_amd64
+export GOARCH=amd64
+export GOOS=linux
+export GOPATH=/golang
+export PATH=\$PATH:\$GOBIN:\$GOPKG:\$GOPATH/bin
+END
 
 
 #ubuntu 16
@@ -27,18 +24,20 @@
 #sudo apt-get install golang-go
 
 #centOS
-yum install epel -y
-yum install go -y
+#yum install epel -y
+#yum install go -y
+#
+#
+#
+#cat > /etc/profile <<END
+#
+#export GOROOT=/usr/lib/go
+#export GOBIN=/usr/bin
+#export PATH=$PATH:$GOBIN:$GOROOT
+#END
 
 
 
-cat > /etc/profile <<END
-
-export GOROOT=/usr/lib/go
-export GOBIN=/usr/bin
-export PATH=$PATH:$GOBIN:$GOROOT
-END
 
 source /etc/profile
-
 go version
