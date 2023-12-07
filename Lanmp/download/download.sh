9bfd6fad61ccce58828637f7462ec3a50a87f0d3
andy chen <bootoo@sina.cn>
2023-12-07 15:38:22 +0800

    Update download.sh

--- Lanmp/download/download.sh
+++ Lanmp/download/download.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 array=(
-  #http://oss.aliyuncs.com/aliyunecs/onekey/php_extend/ZendGuardLoader-php-5.3-linux-glibc23-x86_64.tar.gz
-  #http://oss.aliyuncs.com/aliyunecs/onekey/php_extend/ZendGuardLoader-php-5.3-linux-glibc23-i386.tar.gz
-  http://oss.aliyuncs.com/aliyunecs/onekey/php_extend/ZendGuardLoader-70429-PHP-5.4-linux-glibc23-x86_64.tar.gz
-  #http://oss.aliyuncs.com/aliyunecs/onekey/php_extend/ZendGuardLoader-70429-PHP-5.4-linux-glibc23-i386.tar.gz
-  #http://oss.aliyuncs.com/aliyunecs/onekey/libiconv-1.13.1.tar.gz
+  https://support.edokumenty.eu/download/installation/linux-os/ZendGuardLoader-70429-PHP-5.4-linux-glibc23-x86_64.tar.gz
   http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz
-  http://oss.aliyuncs.com/aliyunecs/onekey/zlib-1.2.3.tar.gz
-  http://oss.aliyuncs.com/aliyunecs/onekey/freetype-2.1.10.tar.gz
-  http://download.sourceforge.net/libpng/libpng-1.6.19.tar.gz
-  http://oss.aliyuncs.com/aliyunecs/onekey/libevent-1.4.14b.tar.gz
-  http://oss.aliyuncs.com/aliyunecs/onekey/libmcrypt-2.5.8.tar.gz
-  http://oss.aliyuncs.com/aliyunecs/onekey/pcre-8.12.tar.gz
+  https://src.fedoraproject.org/repo/pkgs/zlib/zlib-1.2.3.tar.gz/debc62758716a169df9f62e6ab2bc634/zlib-1.2.3.tar.gz
+  https://download-mirror.savannah.gnu.org/releases/freetype/freetype-old/freetype-2.1.10.tar.gz
+  https://distfiles.macports.org/libevent/libevent-1.4.14b-stable.tar.gz
+  https://src.fedoraproject.org/repo/pkgs/libmcrypt/libmcrypt-2.5.8.tar.gz/0821830d930a86a5c69110837c55b7da/libmcrypt-2.5.8.tar.gz
+  https://directadmin.spd.co.il/customapache/pcre-8.12.tar.gz
+  https://mirror.sobukus.de/files/src/libpng/libpng-1.6.19.tar.gz
   http://www.ijg.org/files/jpegsrc.v9a.tar.gz
-  http://oss.aliyuncs.com/aliyunecs/onekey/phpMyAdmin-4.1.8-all-languages.zip
-  http://repository.timesys.com/buildsources/o/openssl/openssl-1.0.0c/openssl-1.0.0c.tar.gz
+  https://ftp.openssl.org/source/old/1.0.0/openssl-1.0.0c.tar.gz
   ${conf_wget_apr}
   ${conf_wget_aprUtil}
   ${conf_wget_webServer}
