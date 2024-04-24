49eeaf50d4ea50b5ac4d4aaa12b718b3612e5846
andychen <bootoo@sina.cn>
2024-04-24 15:12:39 +0800

    fix

--- alldump_new.sh
+++ alldump_new.sh
@@ -9,52 +9,59 @@
 
 USER=root
 PASS=''
-HOST=127.0.0.1
-DATA=/data/center_mysql_backup
+SERVERS="127.0.0.1 172.29.16.64"
+BASE=/data/mysql_backup
 BIN=/andydata/server/mysql-5.7.10/bin
-MAX=3 #备份文件保留个数，超过就会清除最早备份的
-
-FILE=$(date +'%Y-%m-%d')
-TIME=$(date +'%H.%M')
-NAME=${FILE}_${TIME}
-SAVEPATH=${DATA}/${NAME}
+MAX=60 #备份文件保留个数，超过就会清除最早备份的
 
 #check
-if [ ! -d "$BIN" ]; then
-  echo "$BIN is not exsit"
+if [ ! -f "$BIN/mysql" ]; then
+  echo "$BIN/mysql is not exsit"
+  exit 1
 fi
 
-#clean
-len=$(ls ${DATA} | wc -l)
-if [ "$len" -gt "$MAX" ]; then
-  del=0
-  number=$(expr $len - $MAX)
-  for dir in `ls ${DATA}`; do
-    if [ "$del" -ge "$number" ]; then
-      break
-    fi
 
-    ((del++))
-    rm -fr ${DATA}/${dir}
+#loop servers
+for HOST in ${SERVERS}; do
+  DATA=${BASE}/${HOST}
+  FILE=$(date +'%Y-%m-%d')
+  TIME=$(date +'%H.%M')
+  NAME=${FILE}_${TIME}
+  SAVEPATH=${DATA}/${NAME}
+
+  #clean
+  len=$(ls ${DATA} | wc -l)
+  if [ "$len" -gt "$MAX" ]; then
+    del=0
+    number=$(expr $len - $MAX)
+    for dir in `ls ${DATA}`; do
+      if [ "$del" -ge "$number" ]; then
+        break
+      fi
+
+      ((del++))
+      rm -fr ${DATA}/${dir}
+    done
+  fi
+
+  mkdir -p ${SAVEPATH}
+
+  #backup
+  datas=$( ${BIN}/mysql -u${USER} -p${PASS} -h${HOST} -e"show databases;" )
+  datas=($datas)
+  unset datas[0]
+
+  for dataname in ${datas[@]};do
+    ${BIN}/mysqldump --single-transaction --no-tablespaces -u${USER} -p${PASS} -h${HOST} ${dataname} > ${SAVEPATH}/${dataname}.sql
   done
-fi
 
-mkdir -p ${SAVEPATH}
-
-#backup
-datas=$( ${BIN}/mysql -u${USER} -p${PASS} -h${HOST} -e"show databases;" )
-datas=($datas)
-unset datas[0]
-
-for dataname in ${datas[@]};do
-  ${BIN}/mysqldump --single-transaction --no-tablespaces -u${USER} -p${PASS} -h${HOST} ${dataname} > ${SAVEPATH}/${dataname}.sql
+  cd ${DATA}
+  size=$(du -s ${NAME} | awk '{print $1}')
+  if [ "$size" -gt 400 ]; then
+    tar -czvf ${NAME}.tar.gz ./${NAME} >>/dev/null
+  fi
+  rm -fr ./${NAME}
+  cd -
 done
 
-cd ${DATA}
-size=$(du -s ${NAME} | awk '{print $1}')
-if [ "$size" -gt 400 ]; then
-  tar -czvf ${NAME}.tar.gz ./${NAME} >>/dev/null
-fi
-rm -fr ./${NAME}
-cd -
 
